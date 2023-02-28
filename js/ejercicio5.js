6e32f450b098bbf5c896917fcda1572b6d998836
jgromerou <jgromerouro@gmail.com>
2023-02-28 19:46:06 -0300

    ejercicio 5 realizado

---
+++ js/ejercicio5.js
@@ -0,0 +1,9 @@
+/*
+5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+*/
+
+let num1 = parseInt(prompt("Ingrese el número 1:"));
+let num2 = parseInt(prompt("Ingrese el número 2:"));
+document.write(num1 + num2);
